c7214ad2f09714a22d6fa36befb688e41a39b109
Matt <mattarbore@gmail.com>
2016-09-21 11:20:03 -0400

    Added strings

---
+++ jsbasics/0.02_strings.js
@@ -0,0 +1,34 @@
+var birthCity = "Fairfax"; // String = array of characters
+var birthState = "Virgina";
+var comma = ", ";
+console.log(birthCity);
+console.log(birthState);
+
+var userName = "mattarbore";
+var password = "123LetMeIn!";
+var tweet = "Kanye is broke again";
+var facebookPost = "I love coding!";
+var istaCaption = "#Awesome";
+
+console.log(userName);
+console.log("        ");
+console.log("Hello World");
+
+//Concatenation
+console.log(birthCity, birthState);
+console.log(birthCity + comma +birthState);
+
+var highSchool = "Carmel High School";
+var graduatedHighSchool = 2012;
+console.log("I graduated from " + highSchool + comma + graduatedHighSchool + ".");
+
+//Create one variable that holds a string that prints out three other variables.
+//The three other variables should be the make, model, and year of your car.
+
+var make = "Hyundai";
+var model = "Elantra";
+var year = "2016";
+var space = " ";
+var makeModelYear = make + space + model + space + year;
+console.log(makeModelYear);
+
