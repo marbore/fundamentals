178d07f5db320b7fa470f189786211ce139cdb37
Matt <mattarbore@gmail.com>
2016-09-21 10:29:57 -0400

    Added store project and javaScript files

---
+++ jsbasics/0.01_integers.js
@@ -0,0 +1,54 @@
+// var num = 7;
+// var newNum = 5; // this is camel casing
+// var newer_num = 10; // this is snake casing
+
+// //console.log() prints to the console
+
+// //operations with console.log
+// //can subtract, multiply, and division
+// console.log(5 + 5);
+// console.log(10-5);
+// console.log(7*5);
+// console.log(200/40);
+
+// //Modulus Operator
+//  console.log(10%5); //returns zero
+//  console.log(4%3); //returns one
+//  console.log(20%4);
+//  console.log(10%3);
+
+//  var a = 5;
+//  var b = 10;
+//  console.log(a*b);
+//  var c = a + b;
+//  console.log(c);
+//  var numOp = 25+100;
+//  console.log(numOp);
+
+var newNum=10;
+newNum = newNum+1;
+console.log(newNum);
+newNum++; //short cut for newNum = newNumb + 1
+console.log(newNum);
+
+newNum= newNum - 1;
+console.log(newNum);
+newNum--;
+console.log(newNum);
+
+newNum = newNum + 10;
+console.log(newNum);
+
+newNum += 10; //short cut for writing newNum = newNum + 10;
+console.log(newNum);
+
+// newNum = newNum - 20; //long hand method
+// console.log(newNum);
+
+newNum -= 20; // shorthand method
+console.log(newNum);
+
+
+
+
+
